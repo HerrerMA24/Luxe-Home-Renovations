9c9069008505d1a21d09d2fe53a2283626ca5da9
HerrerMA24 <herrera.moises2001@gmail.com>
2024-06-20 15:35:00 -0400

    nav bar

--- src/app.py
+++ src/app.py
@@ -1,17 +1,22 @@
 from datetime import datetime, timedelta, timezone
-import os
+
 import jwt
 import jwt.algorithms
-import streamlit as st
+import streamlit as st  #all streamlit commands will be available through the "st" alias
+from streamlit_navigation_bar import st_navbar
 import utils
 from streamlit_feedback import streamlit_feedback
 
-UTC = timezone.utc
+UTC=timezone.utc
 
 # Init configuration
 utils.retrieve_config_from_agent()
 
-st.set_page_config(page_title="Luxe Home Renovations Custom Q Chat")
+st.set_page_config(page_title="Luxe Home Renovations Custom Q Chat") #HTML title
+
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.title("Luxe Home Renovations") #page title
 
 # Define a function to clear the chat history
 def clear_chat_history():
@@ -24,54 +29,63 @@ def clear_chat_history():
     st.session_state["parentMessageId"] = ""
 
 def navigation_bar():
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    style_path = os.path.join(script_dir, "style.css")
+    page = st_navbar(["Home", "Profile", "About"])
+    
+    if page == "Home":
+        # Add your code for the Home page here
+        st.write("Home Page")
+    elif page == "Profile":
+        # Add your code for the Profile page here
+        st.write("Profile Page")
+    elif page == "About":
+        # Add your code for the About page here
+        st.write("About Page")
 
-    with open(style_path) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-    with st.container():
-        col1, col2, col3 = st.columns([1, 2, 1])
-        with col1:
-            st.markdown("""<a href="#" class="navbar-brand">LHR</a>""", unsafe_allow_html=True)
-        with col2:
-            if "token" not in st.session_state:
-                redirect_uri = f"https://{utils.OAUTH_CONFIG['ExternalDns']}/component/streamlit_oauth.authorize_button/index.html"
-                oauth2 = utils.configure_oauth_component()
-                result = oauth2.authorize_button("Authenticate", scope="openid", pkce="S256", redirect_uri=redirect_uri)
-                if result and "token" in result:
-                    st.session_state.token = result.get("token")
-                    st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(st.session_state.token["id_token"])
-                    st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(tz=UTC) + timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
-                    st.rerun()
-            else:
-                st.markdown("""<div class="navbar-center">Authenticated</div>""", unsafe_allow_html=True)
-        with col3:
-            st.markdown("""<a href="#" class="navbar-link">About</a>""", unsafe_allow_html=True)
-
+oauth2 = utils.configure_oauth_component()
 if "token" not in st.session_state:
-    pass
+    # If not, show authorize button
+    redirect_uri = f"https://{utils.OAUTH_CONFIG['ExternalDns']}/component/streamlit_oauth.authorize_button/index.html"
+    
+    result = oauth2.authorize_button("Authenticate",scope="openid", pkce="S256", redirect_uri=redirect_uri)
+
+    if result and "token" in result:
+        # If authorization successful, save token in session state
+        st.session_state.token = result.get("token")
+        # Retrieve the Identity Center token
+        st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(st.session_state.token["id_token"])
+        st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(tz=UTC) + \
+            timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
+        st.rerun()
 else:
     token = st.session_state["token"]
-    refresh_token = token["refresh_token"]  # saving the long lived refresh_token
+    refresh_token = token["refresh_token"] # saving the long lived refresh_token
     user_email = jwt.decode(token["id_token"], options={"verify_signature": False})["email"]
-    if st.button("Refresh Cognito Token"):
+    if st.button("Refresh Cognito Token") :
+        # If refresh token button is clicked or the token is expired, refresh the token
         token = oauth2.refresh_token(token, force=True)
+        # Put the refresh token in the session state as it is not returned by Cognito
         token["refresh_token"] = refresh_token
+        # Retrieve the Identity Center token
+
         st.session_state.token = token
         st.rerun()
 
     if "idc_jwt_token" not in st.session_state:
         st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(token["id_token"])
-        st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
+        st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + \
+            timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
     elif st.session_state["idc_jwt_token"]["expires_at"] < datetime.now(UTC):
+        # If the Identity Center token is expired, refresh the Identity Center token
         try:
-            st.session_state["idc_jwt_token"] = utils.refresh_iam_oidc_token(st.session_state["idc_jwt_token"]["refreshToken"])
-            st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
+            st.session_state["idc_jwt_token"] = utils.refresh_iam_oidc_token(
+                st.session_state["idc_jwt_token"]["refreshToken"]
+                )
+            st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + \
+                timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
         except Exception as e:
             st.error(f"Error refreshing Identity Center token: {e}. Please reload the page.")
 
-    col1, col2 = st.columns([1, 1])
+    col1, col2 = st.columns([1,1])
 
     with col1:
         st.write("Welcome: ", user_email)
@@ -100,10 +114,12 @@ else:
     if "input" not in st.session_state:
         st.session_state.input = ""
 
+
     # Display the chat messages
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.write(message["content"])
+
 
     # User-provided prompt
     if prompt := st.chat_input():
@@ -111,12 +127,15 @@ else:
         with st.chat_message("user"):
             st.write(prompt)
 
+
     # If the last message is from the user, generate a response from the Q_backend
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
                 placeholder = st.empty()
-                response = utils.get_queue_chain(prompt, st.session_state["conversationId"], st.session_state["parentMessageId"], st.session_state["idc_jwt_token"]["idToken"])
+                response = utils.get_queue_chain(prompt,st.session_state["conversationId"],
+                                                 st.session_state["parentMessageId"],
+                                                 st.session_state["idc_jwt_token"]["idToken"])
                 if "references" in response:
                     full_response = f"""{response["answer"]}\n\n---\n{response["references"]}"""
                 else:
@@ -125,175 +144,25 @@ else:
                 st.session_state["conversationId"] = response["conversationId"]
                 st.session_state["parentMessageId"] = response["parentMessageId"]
 
+
         st.session_state.messages.append({"role": "assistant", "content": full_response})
-        feedback = streamlit_feedback(feedback_type="thumbs", optional_text_label="[Optional] Please provide an explanation")
+        feedback = streamlit_feedback(
+            feedback_type="thumbs",
+            optional_text_label="[Optional] Please provide an explanation",
+        )
 
 def main():
+    # Set the background color
+    page_bg_color = """
+        <style>
+        body {
+            background-color: #F5F5F5; /* Light gray color */
+        }
+        </style>
+    """
+    st.markdown(page_bg_color, unsafe_allow_html=True)
     navigation_bar()
 
 if __name__ == "__main__":
     main()
-# from datetime import datetime, timedelta, timezone
-
-# import jwt
-# import jwt.algorithms
-# import streamlit as st  #all streamlit commands will be available through the "st" alias
-# import utils
-# from streamlit_feedback import streamlit_feedback
-
-# UTC=timezone.utc
-
-# # Init configuration
-# utils.retrieve_config_from_agent()
-
-# st.set_page_config(page_title="Luxe Home Renovations Custom Q Chat") #HTML title
-
-# col1, col2, col3 = st.columns([1, 2, 1])
-# with col2:
-#     st.title("Luxe Home Renovations") #page title
-
-# # Define a function to clear the chat history
-# def clear_chat_history():
-#     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-#     st.session_state.questions = []
-#     st.session_state.answers = []
-#     st.session_state.input = ""
-#     st.session_state["chat_history"] = []
-#     st.session_state["conversationId"] = ""
-#     st.session_state["parentMessageId"] = ""
-
-# # Define the navigation bar function
-# def navigation_bar():
-#     col1, col2, col3 = st.columns(3)
-#     with col1:
-#         st.button("Home")
-#     with col2:
-#         st.button("Profile")
-#     with col3:
-#         st.button("About")
-
-# oauth2 = utils.configure_oauth_component()
-# if "token" not in st.session_state:
-#     # If not, show authorize button
-#     redirect_uri = f"https://{utils.OAUTH_CONFIG['ExternalDns']}/component/streamlit_oauth.authorize_button/index.html"
-    
-#     result = oauth2.authorize_button("Authenticate",scope="openid", pkce="S256", redirect_uri=redirect_uri)
-
-#     if result and "token" in result:
-#         # If authorization successful, save token in session state
-#         st.session_state.token = result.get("token")
-#         # Retrieve the Identity Center token
-#         st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(st.session_state.token["id_token"])
-#         st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(tz=UTC) + \
-#             timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
-#         st.rerun()
-# else:
-#     token = st.session_state["token"]
-#     refresh_token = token["refresh_token"] # saving the long lived refresh_token
-#     user_email = jwt.decode(token["id_token"], options={"verify_signature": False})["email"]
-#     if st.button("Refresh Cognito Token") :
-#         # If refresh token button is clicked or the token is expired, refresh the token
-#         token = oauth2.refresh_token(token, force=True)
-#         # Put the refresh token in the session state as it is not returned by Cognito
-#         token["refresh_token"] = refresh_token
-#         # Retrieve the Identity Center token
-
-#         st.session_state.token = token
-#         st.rerun()
-
-#     if "idc_jwt_token" not in st.session_state:
-#         st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(token["id_token"])
-#         st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + \
-#             timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
-#     elif st.session_state["idc_jwt_token"]["expires_at"] < datetime.now(UTC):
-#         # If the Identity Center token is expired, refresh the Identity Center token
-#         try:
-#             st.session_state["idc_jwt_token"] = utils.refresh_iam_oidc_token(
-#                 st.session_state["idc_jwt_token"]["refreshToken"]
-#                 )
-#             st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + \
-#                 timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
-#         except Exception as e:
-#             st.error(f"Error refreshing Identity Center token: {e}. Please reload the page.")
-
-#     col1, col2 = st.columns([1,1])
-
-#     with col1:
-#         st.write("Welcome: ", user_email)
-#     with col2:
-#         st.button("Clear Chat History", on_click=clear_chat_history)
-
-#     # Initialize the chat messages in the session state if it doesn't exist
-#     if "messages" not in st.session_state:
-#         st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-#     if "conversationId" not in st.session_state:
-#         st.session_state["conversationId"] = ""
-
-#     if "parentMessageId" not in st.session_state:
-#         st.session_state["parentMessageId"] = ""
-
-#     if "chat_history" not in st.session_state:
-#         st.session_state["chat_history"] = []
-
-#     if "questions" not in st.session_state:
-#         st.session_state.questions = []
-
-#     if "answers" not in st.session_state:
-#         st.session_state.answers = []
-
-#     if "input" not in st.session_state:
-#         st.session_state.input = ""
-
-
-#     # Display the chat messages
-#     for message in st.session_state.messages:
-#         with st.chat_message(message["role"]):
-#             st.write(message["content"])
-
-
-#     # User-provided prompt
-#     if prompt := st.chat_input():
-#         st.session_state.messages.append({"role": "user", "content": prompt})
-#         with st.chat_message("user"):
-#             st.write(prompt)
-
-
-#     # If the last message is from the user, generate a response from the Q_backend
-#     if st.session_state.messages[-1]["role"] != "assistant":
-#         with st.chat_message("assistant"):
-#             with st.spinner("Thinking..."):
-#                 placeholder = st.empty()
-#                 response = utils.get_queue_chain(prompt,st.session_state["conversationId"],
-#                                                  st.session_state["parentMessageId"],
-#                                                  st.session_state["idc_jwt_token"]["idToken"])
-#                 if "references" in response:
-#                     full_response = f"""{response["answer"]}\n\n---\n{response["references"]}"""
-#                 else:
-#                     full_response = f"""{response["answer"]}\n\n---\nNo sources"""
-#                 placeholder.markdown(full_response)
-#                 st.session_state["conversationId"] = response["conversationId"]
-#                 st.session_state["parentMessageId"] = response["parentMessageId"]
-
-
-#         st.session_state.messages.append({"role": "assistant", "content": full_response})
-#         feedback = streamlit_feedback(
-#             feedback_type="thumbs",
-#             optional_text_label="[Optional] Please provide an explanation",
-#         )
-
-# def main():
-#     # Set the background color
-#     page_bg_color = """
-#         <style>
-#         body {
-#             background-color: #F5F5F5; /* Light gray color */
-#         }
-#         </style>
-#     """
-#     st.markdown(page_bg_color, unsafe_allow_html=True)
-#     navigation_bar()
-
-# if __name__ == "__main__":
-#     main()
     
